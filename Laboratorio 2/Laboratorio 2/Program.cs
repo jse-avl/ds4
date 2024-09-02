45d8a2b5877d41f276c34bc60ed1f62d9b4c9968
IvnAvi <avilajoser8@gmail.com>
2024-09-01 21:54:36 -0500

    Laboratorio 2 completo

---
+++ Laboratorio 2/Laboratorio 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Laboratorio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            // Ejemplo utilizando las variables de instancia de clase
+            client.FirstName = "Jose";
+            client.LastName = "Avila";
+            client.Age = 21;
+            client.ID = 1;
+
+            Console.WriteLine(client.GetFullName());
+        }
+    }
+
+    public class Client
+    {
+        // Declarando variables de instancia en la clase
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ushort Age { get; set; }
+
+        public string GetFullName()
+        {
+            // Utilizando variables de instancia dentro de métodos de la clase
+            return FirstName + " " + LastName;
+        }
+    }
+}
