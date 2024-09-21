13a4d43bb8ac3beef8d653936ceb83268e4256f5
IvnAvi <avilajoser8@gmail.com>
2024-09-21 18:54:43 -0500

    Laboratorio 6 completo

---
+++ Laboratorio 6/Laboratorio 6-4/Program.cs
@@ -0,0 +1,18 @@
+﻿class Prograam
+{
+    static void checkAge (int age)
+    {
+        if (age > 18) 
+        {
+            throw new ArithmeticException("Acceso negado - No cumple con el criterio de edad");
+        }
+        else
+        {
+            Console.WriteLine("Acceso concedido");
+        }
+    }
+    static void Main(string[] args)
+    {
+        checkAge(18);
+    }
+}
