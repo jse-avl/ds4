797f9786e7f7c7d7798b5d31e1133fbf9763cd87
IvnAvi <avilajoser8@gmail.com>
2024-09-21 22:31:03 -0500

    Laboratorio 7 completo

---
+++ Laboratorio 7/Laboratorio 7/Cliente.cs
@@ -0,0 +1,31 @@
+﻿class Cliente
+{
+    private string nombre;
+    private int monto;
+
+    public Cliente(string nom)
+    {
+        nombre = nom;
+        monto = 0;
+    }
+
+    public void Depositar(int m)
+    {
+        monto = monto + m;
+    }
+
+    public void Retirar(int m)
+    {
+        monto = monto - m;
+    }
+
+    public int ObtenerSaldo()
+    {
+        return monto;
+    }
+
+    public void Imprimir()
+    {
+        Console.WriteLine(nombre + " tiene depositado la suma de " + monto);
+    }
+}
