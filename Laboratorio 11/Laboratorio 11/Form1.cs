22ffce163b04b53f5f17877a34dc385b0d672a42
IvnAvi <avilajoser8@gmail.com>
2024-10-02 21:16:58 -0500

    Laboratorio 11 completo

---
+++ Laboratorio 11/Laboratorio 11/Form1.cs
@@ -0,0 +1,20 @@
+namespace Laboratorio_11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblHelloWorld.Text = "Hello World!";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
